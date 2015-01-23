f9ef3db8ab80168e5ec27cb36b2517b12b943df8
tomaszjureczko <tomasz.jureczko@gmail.com>
2015-01-23 16:22:34 +0000

    pagination

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var app = express();
 
-var socialFeed = {
-  "items": []
-}
+var items = [];
 
 for(var i=0; i < 100; i++) {
   var item = {};
@@ -13,11 +11,15 @@ for(var i=0; i < 100; i++) {
   item.image = "http://photos-d.ak.instagram.com/hphotos-ak-xaf1/t51.2885-15/10914250_444222172391539_115478886_n.jpg";
   item.text = "post with id " + i;
   item.epoch = "1421586217";
-  socialFeed.items.push(item);
+  items.push(item);
 }
 
 app.get('/social-feed', function(req, res){
-  res.jsonp(socialFeed)
+  var items_to_return = parseInt(req.param('items_to_return', 20));
+  var return_posts_after_post_id = parseInt(req.param('return_posts_after_post_id', -1));
+  var response = {};
+  response.items = items.slice(return_posts_after_post_id + 1, return_posts_after_post_id + 1 +items_to_return);
+  res.jsonp(response);
 });
 
 var port = process.env.PORT || 3000;
